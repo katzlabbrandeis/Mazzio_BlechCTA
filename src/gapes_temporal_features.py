827533506fea64ad93f758200d72bf273dd8d9f4
Abuzar Mahmood <abuzarmahmood@gmail.com>
2026-04-02 17:17:13 -0400

    feat(temporal_features): enhance data processing and visualization
    - Created an artifacts directory to store intermediate results.
    - Saved sorted features to `.npy` files under the artifacts directory.
    - Added standard deviation computation and visualization for binned features.
    - Enhanced PCA analysis including cumulative explained variance and 3D visualization of binned features.
    - Updated the plot layout for better visual comparison of mean and standard deviation of binned features.
    - Saved multiple new figures relevant to binned features and their PCA projections.

--- src/gapes_temporal_features.py
+++ src/gapes_temporal_features.py
@@ -11,7 +11,9 @@ base_dir = '/home/abuzarmahmood/projects/CMazzio_analysis'
 # data_dir = '/home/abuzarmahmood/projects/CMazzio_analysis/data/changepoint_gapes'
 data_dir = os.path.join(base_dir, 'data', 'changepoint_gapes')
 plot_dir = os.path.join(base_dir, 'plots', 'changepoint_gapes')
+artifacts_dir = os.path.join(base_dir, 'artifacts', 'changepoint_gapes')
 os.makedirs(plot_dir, exist_ok=True)
+os.makedirs(artifacts_dir, exist_ok=True)
 
 
 file_list = os.listdir(data_dir) 
@@ -45,6 +47,10 @@ sort_inds = np.argsort(y)
 sorted_y = y[sort_inds]
 sorted_X = X_scaled[sort_inds]
 
+# Write out as artifacts
+np.save(os.path.join(artifacts_dir, 'sorted_y.npy'), sorted_y)
+np.save(os.path.join(artifacts_dir, 'sorted_X.npy'), sorted_X)
+
 fig, ax = plt.subplots(figsize=(4, 6))
 ax.pcolormesh(
     np.arange(sorted_X.shape[1]),
@@ -77,36 +83,53 @@ bin_size = 50
 bins = np.arange(y.min(), y.max() + bin_size, bin_size)
 
 binned_X = []
+binned_X_std = []
 wanted_inds = np.digitize(y, bins) - 1
 for i in range(len(bins) - 1):
     inds = np.where(wanted_inds == i)[0]
     if len(inds) > 0:
-        binned_X.append(X_scaled[inds].mean(axis=0))
+        wanted_X = X_scaled[inds]
+        binned_X.append(wanted_X.mean(axis=0))
+        binned_X_std.append(wanted_X.std(axis=0))
 
 binned_X = np.array(binned_X)
+binned_X_std = np.array(binned_X_std)
 
-fig, ax = plt.subplots(figsize=(4, 6))
-im = ax.pcolormesh(
+fig, ax = plt.subplots(1,2, figsize=(8, 4))
+im = ax[0].pcolormesh(
     np.arange(binned_X.shape[1]),
     bins[:-1],
     binned_X,
     shading='auto',
     cmap='jet'
     )
-ax.set_xlabel('Feature Index')
-ax.set_ylabel('Binned Start Time')
-ax.set_title('Binned Temporal Evolution of Features')
-plt.colorbar(im, label='Average Feature Value (Standardized)')
+im2 = ax[1].pcolormesh(
+    np.arange(binned_X_std.shape[1]),
+    bins[:-1],
+    binned_X_std,
+    shading='auto',
+    cmap='jet'
+    )
+ax[0].set_xlabel('Feature Index')
+ax[0].set_ylabel('Binned Start Time')
+ax[0].set_title('Feature Mean')
+ax[1].set_xlabel('Feature Index')
+ax[1].set_title('Feature Std Dev')
+plt.colorbar(im, ax=ax[0], label='Mean Feature Value (Standardized)')
+plt.colorbar(im2, ax=ax[1], label='Std Dev of Feature Value (Standardized)')
 # plt.show()
 fig.savefig(os.path.join(plot_dir, 'binned_temporal_evolution_features.png'), dpi=300)
 plt.close()
 
+
 # Make lineplots of binned features
+smoothed_binned_X = []
 fig, ax = plt.subplots(X.shape[1], 1, figsize=(5, 10), sharex=True)
 for i in range(X.shape[1]):
     ax[i].plot(bins[:-1], binned_X[:, i], '.-', label='Actual')
     # Also plot smoothed version of the line
     smoothed = savgol_filter(binned_X[:, i], 5, 2)
+    smoothed_binned_X.append(smoothed)
     ax[i].plot(bins[:-1], smoothed, 'r-', alpha=0.7, label='Smoothed')
     ax[i].set_ylabel(f'Feature {i}')
 ax[-1].set_xlabel('Binned Start Time')
@@ -116,6 +139,67 @@ plt.legend(loc='lower center', bbox_to_anchor=(0.5, -1), ncol=2)
 plt.tight_layout()
 # plt.show()
 fig.savefig(os.path.join(plot_dir, 'binned_temporal_evolution_individual_features.png'), dpi=300)
+plt.close()
+
+# Perform PCA on smoothed binned features to check for correlations
+smoothed_binned_X = np.array(smoothed_binned_X).T
+pca = PCA()
+pca.fit(smoothed_binned_X)
+explained_variance = pca.explained_variance_ratio_
+fig, ax = plt.subplots(figsize=(5, 5))
+# Start plot at 0
+ax.plot(
+        np.arange(0, len(explained_variance)+1),
+        np.concatenate(([0], np.cumsum(explained_variance))),
+        marker='o'
+        )
+ax.set_xlabel('Number of Principal Components')
+ax.set_ylabel('Cumulative Explained Variance')
+fig.suptitle('PCA Explained Variance (Smoothed Binned Features)')
+plt.grid()
+# plt.show()
+fig.savefig(os.path.join(plot_dir, 'pca_explained_variance_smoothed_binned_features.png'), dpi=300)
+plt.close()
+
+# Plot smoothed binned features in 3D PCA 
+pca = PCA(n_components=3)
+pca.fit(smoothed_binned_X.T)
+smoothed_binned_X_pca = pca.transform(smoothed_binned_X.T).T
+
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+im = ax.scatter(
+        smoothed_binned_X_pca[0], 
+        smoothed_binned_X_pca[1], 
+        smoothed_binned_X_pca[2], 
+        c=bins[:-1], cmap='viridis', 
+        edgecolor='k', alpha=0.7, s=100)
+# Also plot smoothed version of the line
+ax.plot(smoothed_binned_X_pca[0], smoothed_binned_X_pca[1], smoothed_binned_X_pca[2], 
+        color='k', alpha=0.3, label='Smoothed Binned Features')
+ax.set_xlabel('Smoothed Binned Feature 1')
+ax.set_ylabel('Smoothed Binned Feature 2')
+ax.set_zlabel('Smoothed Binned Feature 3')
+ax.set_title('Smoothed Binned Feature Space')
+plt.colorbar(im, label='Binned Start Time')
+# plt.show()
+fig.savefig(os.path.join(plot_dir, 'smoothed_binned_feature_space.png'), dpi=300)
+plt.close()
+
+# Use same PCA projection to plot original features in the same space
+X_pca = pca.transform(X_scaled).T
+fig, ax = plt.subplots()
+im = ax.scatter(
+        X_pca[0],
+        X_pca[1],
+        c=y, cmap='viridis',
+        edgecolor='k', alpha=0.7, s=100)
+ax.set_xlabel('PCA Feature 1')
+ax.set_ylabel('PCA Feature 2')
+ax.set_title('Original Feature Space in PCA Projection')
+plt.colorbar(im, label='Start Time')
+# plt.show()
+fig.savefig(os.path.join(plot_dir, 'original_feature_space_pca_projection.png'), dpi=300)
 plt.close()
 
 ##############################
