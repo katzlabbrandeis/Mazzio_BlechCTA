e2ec3e5eb3292f0d8b419ca64df399ee241b100b
Abuzar Mahmood <abuzarmahmood@gmail.com>
2026-04-10 16:41:44 -0400

    feat(gapes_temporal_features): extend analysis with significance testing and feature extraction
    - Added functionality to test for significance of regression of features on time using linear regression, storing p-values in a CSV.
    - Modified the data loading logic with a boolean flag to control reloading.
    - Improved error handling for the presence of necessary directories and files.
    - Added PCA loadings visualization and saved the plots for analysis.
    - Organized and refactored the code for clarity and maintainability.

--- src/gape_temporal_difference/gapes_temporal_features.py
+++ src/gape_temporal_difference/gapes_temporal_features.py
@@ -3,53 +3,65 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.signal import savgol_filter
+from scipy.stats import linregress
 
 base_dir = '/home/abuzarmahmood/projects/CMazzio_analysis'
 # data_dir = '/home/abuzarmahmood/projects/CMazzio_analysis/data/changepoint_gapes'
 data_dir = os.path.join(base_dir, 'data', 'changepoint_gapes')
 plot_dir = os.path.join(base_dir, 'plots', 'changepoint_gapes')
 artifacts_dir = os.path.join(base_dir, 'artifacts', 'changepoint_gapes')
-os.makedirs(plot_dir, exist_ok=True)
-os.makedirs(artifacts_dir, exist_ok=True)
+# os.makedirs(plot_dir, exist_ok=True)
+# os.makedirs(artifacts_dir, exist_ok=True)
+# Make sure directories exist
+for directory in [data_dir, plot_dir, artifacts_dir]:
+    if not os.path.exists(directory):
+        print(f"Directory {directory} does not exist. Please create it and add the necessary files.")
 
 
-file_list = os.listdir(data_dir) 
-# Load pkls
-dfs = []
-for file in file_list:
-    if file.endswith('.pkl'):
-        df = pd.read_pickle(os.path.join(data_dir, file))
-        dfs.append(df)
+reload_data_bool = False
+############################################################
+if reload_data_bool:
+    file_list = os.listdir(data_dir) 
+    # Load pkls
+    dfs = []
+    for file in file_list:
+        if file.endswith('.pkl'):
+            df = pd.read_pickle(os.path.join(data_dir, file))
+            dfs.append(df)
 
-final_df = pd.concat(dfs, ignore_index=True)
+    final_df = pd.concat(dfs, ignore_index=True)
 
-y = final_df['start_time'].values
-X = final_df[[x for x in final_df.columns if 'feat' in x]].values
+    y = final_df['start_time'].values
+    X = final_df[[x for x in final_df.columns if 'feat' in x]].values
 
-# Check that features are standardized
-# X.mean(axis=0)
-# X.std(axis=0)
+    # Check that features are standardized
+    # X.mean(axis=0)
+    # X.std(axis=0)
 
-# Standardize features
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+    # Standardize features
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
 
-# recheck
-print(X_scaled.mean(axis=0))
-print(X_scaled.std(axis=0))
+    # recheck
+    print(X_scaled.mean(axis=0))
+    print(X_scaled.std(axis=0))
 
-# Sort by y and plot
-sort_inds = np.argsort(y)
-sorted_y = y[sort_inds]
-sorted_X = X_scaled[sort_inds]
+    # Sort by y and plot
+    sort_inds = np.argsort(y)
+    sorted_y = y[sort_inds]
+    sorted_X = X_scaled[sort_inds]
 
-# Write out as artifacts
-np.save(os.path.join(artifacts_dir, 'sorted_y.npy'), sorted_y)
-np.save(os.path.join(artifacts_dir, 'sorted_X.npy'), sorted_X)
+    # Write out as artifacts
+    np.save(os.path.join(artifacts_dir, 'sorted_y.npy'), sorted_y)
+    np.save(os.path.join(artifacts_dir, 'sorted_X.npy'), sorted_X)
+############################################################
+
+sorted_X = np.load(os.path.join(artifacts_dir, 'sorted_X.npy'))
+sorted_y = np.load(os.path.join(artifacts_dir, 'sorted_y.npy'))
 
 fig, ax = plt.subplots(figsize=(4, 6))
 ax.pcolormesh(
@@ -223,35 +235,35 @@ plt.grid()
 fig.savefig(os.path.join(plot_dir, 'pca_explained_variance.png'), dpi=300)
 plt.close()
 
-##############################
-# Use a 2 layered network to predict y from X
-# The second layer will allow us to extract a relevant feature space that is predictive of y
-# Check cross-validation performance to ensure we're not overfitting
-
-mlp = MLPRegressor(hidden_layer_sizes=(3), max_iter=10000, random_state=42, activation='relu', verbose=True)
-from sklearn.model_selection import cross_val_score
-scores = cross_val_score(mlp, X_scaled, y, cv=5, scoring='r2')
-
-# Extract the relevant feature space from the second layer
-mlp.fit(X_scaled, y)
-# Get the weights of the second layer
-weights = mlp.coefs_[0]
-intercepts = mlp.intercepts_[0]
-# Project the original features onto the weights to get the new feature space
-new_features = X_scaled @ weights + intercepts
-# Rectify the new features (since we used ReLU)
-new_features = np.maximum(0, new_features)
-
-# Plot the new features in 3D space and color by y
-fig = plt.figure(figsize=(8, 6))
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(new_features[:, 0], new_features[:, 1], new_features[:, 2], c=y, cmap='viridis')
-ax.set_xlabel('New Feature 1')
-ax.set_ylabel('New Feature 2')
-ax.set_zlabel('New Feature 3')
-ax.set_title('New Feature Space from MLP')
-plt.colorbar(label='Start Time')
-plt.show()
+# ##############################
+# # Use a 2 layered network to predict y from X
+# # The second layer will allow us to extract a relevant feature space that is predictive of y
+# # Check cross-validation performance to ensure we're not overfitting
+#
+# mlp = MLPRegressor(hidden_layer_sizes=(3), max_iter=10000, random_state=42, activation='relu', verbose=True)
+# from sklearn.model_selection import cross_val_score
+# scores = cross_val_score(mlp, X_scaled, y, cv=5, scoring='r2')
+#
+# # Extract the relevant feature space from the second layer
+# mlp.fit(X_scaled, y)
+# # Get the weights of the second layer
+# weights = mlp.coefs_[0]
+# intercepts = mlp.intercepts_[0]
+# # Project the original features onto the weights to get the new feature space
+# new_features = X_scaled @ weights + intercepts
+# # Rectify the new features (since we used ReLU)
+# new_features = np.maximum(0, new_features)
+#
+# # Plot the new features in 3D space and color by y
+# fig = plt.figure(figsize=(8, 6))
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(new_features[:, 0], new_features[:, 1], new_features[:, 2], c=y, cmap='viridis')
+# ax.set_xlabel('New Feature 1')
+# ax.set_ylabel('New Feature 2')
+# ax.set_zlabel('New Feature 3')
+# ax.set_title('New Feature Space from MLP')
+# plt.colorbar(label='Start Time')
+# plt.show()
 
 ############################################################
 # A lot of variability seems to be present in the first 200ms
@@ -263,6 +275,23 @@ cut_inds = sorted_y > cutoff_time
 
 cut_y = sorted_y[cut_inds] 
 cut_X = sorted_X[cut_inds] 
+
+##############################
+# Test for significance of regression of features on time using a simple linear regression for each feature
+p_values = []
+for i in range(cut_X.shape[1]):
+    slope, intercept, r_value, p_value, std_err = linregress(cut_y, cut_X[:, i])
+    p_values.append(p_value)
+
+# Write out as a csv
+p_values_df = pd.DataFrame({
+    'feature_index': np.arange(cut_X.shape[1]),
+    'p_value': p_values
+    })
+p_values_df.to_csv(os.path.join(artifacts_dir, 'feature_time_regression_p_values.csv'), index=False)
+
+
+##############################
 
 fig, ax = plt.subplots(cut_X.shape[1], 1, figsize=(5, 10), sharex=True)
 for i in range(cut_X.shape[1]):
@@ -328,6 +357,19 @@ cut_smoothed_binned_X = np.array(cut_smoothed_binned_X).T
 pca = PCA(n_components=3)
 pca.fit(cut_smoothed_binned_X)
 cut_binned_X_pca = pca.transform(cut_smoothed_binned_X).T
+
+# Get loadings of the first 3 PCs
+loadings = pca.components_.T
+# feature_sum = np.sum(np.abs(loadings), axis=1)
+# Plot loadings as a heatmap
+fig, ax = plt.subplots(figsize=(5, 10))
+im = ax.imshow(np.abs(loadings), aspect='auto', cmap='viridis')
+ax.set_xlabel('Principal Component')
+ax.set_ylabel('Original Feature Index')
+ax.set_title('Absolute PCA Loadings (Cutoff at 200ms)')
+plt.colorbar(im, label='Loading Value')
+fig.savefig(os.path.join(plot_dir, 'pca_loadings_cutoff_200ms.png'), dpi=300)
+plt.close()
 
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection='3d')
